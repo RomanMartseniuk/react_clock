e4326ab53df967b9d5356fcfd49589135e197b44
Roman Martseniuk <roman.martseniuk.developer@gmail.com>
2024-11-05 12:11:11 +0200

    complete 1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
+import { Clock } from './component/Clock/Clock';
 
 function getRandomName(): string {
   const value = Date.now().toString().slice(-4);
@@ -8,30 +9,41 @@ function getRandomName(): string {
 }
 
 export const App: React.FC = () => {
-  const today = new Date();
-  let clockName = 'Clock-0';
+  const [hasClock, setHasClock] = useState(true);
+  const [clockName, setClockName] = useState('Clock-0');
 
-  // This code starts a timer
-  const timerId = window.setInterval(() => {
-    clockName = getRandomName();
-  }, 3300);
+  useEffect(() => {
+    const handleContextMenu = (event: MouseEvent) => {
+      event.preventDefault(); // not to show the context menu
+      setHasClock(false);
+    };
 
-  // this code stops the timer
-  window.clearInterval(timerId);
+    const handleClick = () => {
+      setHasClock(true);
+    };
+
+    const timerId = window.setInterval(() => {
+      const newName = getRandomName();
+
+      // eslint-disable-next-line no-console
+      //console.warn(`Renamed from ${clockName} to ${newName}`);
+      setClockName(newName);
+    }, 3300);
+
+    document.addEventListener('contextmenu', handleContextMenu);
+    document.addEventListener('click', handleClick);
+
+    return () => {
+      window.clearInterval(timerId);
+      document.removeEventListener('contextmenu', handleContextMenu);
+      document.removeEventListener('click', handleClick);
+    };
+  }, [clockName]);
 
   return (
     <div className="App">
       <h1>React clock</h1>
-
-      <div className="Clock">
-        <strong className="Clock__name">{clockName}</strong>
-
-        {' time is '}
-
-        <span className="Clock__time">
-          {today.toUTCString().slice(-12, -4)}
-        </span>
-      </div>
+      {hasClock && <Clock getRandomName={getRandomName} name={clockName} />}
     </div>
   );
 };
